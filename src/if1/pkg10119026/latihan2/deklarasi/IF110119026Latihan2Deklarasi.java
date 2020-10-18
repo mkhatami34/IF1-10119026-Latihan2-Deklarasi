5b94c6c08431317feb5ac0282462753f6d7a4b04
mkhatami34 <koabarab361@gmail.com>
2020-10-18 19:58:08 +0700

    initial commit

---
+++ src/if1/pkg10119026/latihan2/deklarasi/IF110119026Latihan2Deklarasi.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119026.latihan2.deklarasi;
+
+/**
+ *
+ * @author Asus
+ * NAMA     : Muhammad Khatami
+ * KELAS    : PBO
+ * NIM      : 10119026
+ * Deskripsi Program : Program ini berisi program untuk menampilkan deklarasi
+ * & pengaksesan variabel
+ */
+public class IF110119026Latihan2Deklarasi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+        
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
